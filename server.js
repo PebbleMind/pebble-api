6f129f38710708a46371151810386ea9284fd074
PraveenM24 <praveen050800@gmail.com>
2021-06-28 18:39:09 +0530

    CORS #2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(helmet());
 app.use(compression());
 app.use("/public", express.static(process.cwd() + "/public"));
-app.use(cors({ origin: "https://pebble-test.herokuapp.com/" }));
+app.use(cors());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
