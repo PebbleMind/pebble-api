f5ae357544177228bec1eebf3165fb8f30c1ca36
PraveenM24 <praveen050800@gmail.com>
2021-08-26 13:58:32 +0530

    / #Refactor

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.route('/')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/index.html');
 });
+app.listen(process.env.PORT)
 
 mongoose.connect(
     process.env.MONGODB_URI,
@@ -43,7 +44,3 @@ mongoose.connect(
         );
     },
 );
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-    console.log('Your app is listening on port ' + listener.address().port)
-})  
